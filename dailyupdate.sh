6bfe6a0df3ee7c475e832a21c820a2802ecbcf5c
natelawson73 <natelawson73@gmail.com>
2022-08-30 15:13:22 -0500

    daily jupyter update from $USER@$(hostname -s) on $(date)

--- dailyupdate.sh
+++ dailyupdate.sh
@@ -2,8 +2,8 @@
 cp script.log /home/nlawson/scriptlog/script.log_$(date '+%Y%m%d%s') 
 message='daily jupyter update from $USER@$(hostname -s) on $(date)'
 git add . >script.log 2>&1; echo 'exit code' $? >> script.log
-git commit -m "$message"  >>script.log 2>&1
-echo 'exit code' $? >> script.log
-git push origin devbranch >>script.log 2>&1
-echo 'exit code' $? >> script.log
+git commit -m "$message"; echo 'exit code' $? >> script.log
+#echo 'exit code' $? >> script.log
+git push origin devbranch >>script.log 2>&1; echo 'exit code' $? >> script.log
+#echo 'exit code' $? >> script.log
 
