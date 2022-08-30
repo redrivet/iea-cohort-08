8e5b4437e85a05dd31a48de2ba45b2528839ce59
natelawson73 <natelawson73@gmail.com>
2022-08-30 14:58:46 -0500

    daily jupyter update from $USER@$(hostname -s) on $(date)

--- dailyupdate.sh
+++ dailyupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 cp script.log /home/nlawson/script.log_$(date (+ ''%Y%m%d%s))
-message="daily jupyter update from $USER@$(hostname -s) on $(date)"
+message='daily jupyter update from $USER@$(hostname -s) on $(date)'
 #script.log 2>&1
 git add . >script.log 2>&1
 echo 'exit code' $? >> script.log
