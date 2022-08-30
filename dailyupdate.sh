107f2842cf61ee266f26ebcda1c3382f49c6d1d7
natelawson73 <natelawson73@gmail.com>
2022-08-30 14:49:58 -0500

    daily jupyter update from CORP\nlawson@a-3cpj9aewpgn6s on Tue Aug 30 14:49:58 CDT 2022

--- dailyupdate.sh
+++ dailyupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-mv script.log /home/nlawson/scriptlog_&&date
+mv script.log /home/nlawson/script.log_$date
 message="daily jupyter update from $USER@$(hostname -s) on $(date)"
 #script.log 2>&1
 git add . >script.log 2>&1
@@ -7,5 +7,5 @@ echo $? >> script.log
 git commit -m "$message"  >>script.log 2>&1
 echo $? >> script.log
 git push origin devbranch >>script.log 2>&1
-echo $? >> script.log
+echo 'exit code' $? >> script.log
 
