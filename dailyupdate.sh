496da407bba4a67fbc24559ae80e24e347c62b6b
natelawson73 <natelawson73@gmail.com>
2022-09-01 08:07:22 -0500

    Updated juypter on 2022-09-01 08:07:22

--- dailyupdate.sh
+++ dailyupdate.sh
@@ -1,27 +1,31 @@
 #!/bin/bash 
 cp script.log "/home/nlawson/scriptlog/script.log_$(date +'%Y%m%d-%H%M%S')"
 message="Updated juypter on `date +'%Y-%m-%d %H:%M:%S'`"
-git add . >script.log 2>&1; echo 'exit code' $? >> script.log
+git add . >script.log 2>&1 >> script.log
 if [ $? -eq 0 ]
 then 
   echo "add completed successfully"
 else 
+  echo 'exit code' $?
   echo "add failed, check log"
   exit 1
 fi
-git commit -m "$message"; echo 'exit code' $? >> script.log
+git commit -m "$message" >> script.log
+#git commit -m "Message" GIT_SUCCESS=$?
 if [ $? -eq 0 ]
 then 
   echo "commit completed successfully"
 else 
+  echo 'exit code' $? 
   echo "add failed, check log"
-  exit 
+  exit 1
 fi
-git push origin devbranch >>script.log 2>&1; echo 'exit code' $? >> script.log
+git push origin devbranch >>script.log 2>&1 >> script.log
 if [ $? -eq 0 ]
 then 
   echo "push completed successfully"
 else 
+  echo 'exit code'
   echo "add failed, check log"
   exit 1
 fi
