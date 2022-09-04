a2bd51b4a41c0fd820e2c5e19b08b11be9e721a3
curtis-roy <curtis.roy@libertymutual.com>
2022-09-04 11:57:34 -0700

    Add work from Sunday

--- 04_intro_to_programming/check_prime.py
+++ 04_intro_to_programming/check_prime.py
@@ -2,4 +2,13 @@
 
 """Get a list of prime numbers in a range"""
 
-check_num = 
+lower = int(input('Enter the lower end of the range: '))
+upper = int(input('Enter the upper end of the range: '))
+
+for number in range(lower,upper):
+    if number > 1:
+        for item in range (2,number):
+            if number % item == 0:
+                break
+        else:
+            print(number)
