8cdfd021495b76dafdcb65e2ecc2f827ce63872b
leblanck <kyle@leblanc.sh>
2022-09-21 13:47:34 -0400

    lab: working on file IO

---
+++ 05_python_for_devops/file_io.py
@@ -0,0 +1,24 @@
+#in_file_name = input('Please enter an input filename... ')
+in_file_name = 'poem.txt'
+#out_file_name = input('Please enter an output filename... ')
+out_file_name = 'out_file.txt'
+
+# Original Working Blocks
+# with open(in_file_name, 'r') as f_in:
+#     input_file = f_in.readlines()
+#     input_file.reverse()
+    
+#     with open(out_file_name, 'w') as f_out:
+#         output_file = f_out.writelines(input_file)
+
+
+# Merged into single with block
+# with open(in_file_name, 'r') as f_in, open (out_file_name, 'w') as f_out:
+#     input_file = f_in.readlines()
+#     input_file.reverse()
+#     f_out.writelines(input_file)
+#     f_out.close()
+
+# EVEN SHORTER, PYTHONIC FTW
+with open(in_file_name, 'r') as f_in, open (out_file_name, 'w') as f_out:
+    f_out.writelines(reversed(f_in.readlines()))
