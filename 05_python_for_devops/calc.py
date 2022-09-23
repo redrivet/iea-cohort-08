fd9d82e8bfee430b5979ee51c8ad34785562d1a1
leblanck <kyle@leblanc.sh>
2022-09-23 14:22:08 -0400

    lab: work with modules, functions

---
+++ 05_python_for_devops/calc.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Calculate Funciton - used to calculate 2 values based on given operator.
+"""
+from math import log
+
+def calculate(op1, op2, operator='+'):
+    """
+    Takes in two operands (op1, op2) and an operator (+, -, *, /)
+    to perform a calculation. Exception for Zero Dision handled below.
+
+    Returns answer.
+    """
+    try:
+        int_1 = int(op1)
+        int_2 = int(op2)
+    except ValueError:
+        print('You must enter integers for param1 and param2.')
+        return None
+
+    if operator == '-':
+        answer = int_1 - int_2
+    elif operator == 'log':
+        try:
+            answer = log(int_1) / log(int_2)
+        except ValueError:
+            print('Cannot log 0')
+            answer = None
+    elif operator == '/':
+        try:
+            answer = int_1 / int_2
+        except ZeroDivisionError:
+            print('You cannot divide by 0!')
+            answer = 0
+    elif operator == '*':
+        answer = int_1 * int_2
+    elif operator == '+':
+        answer = int_1 + int_2
+
+    return answer
