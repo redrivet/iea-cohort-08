3ac09561203d5229519da1a056e5d00358045949
natelawson73 <natelawson73@gmail.com>
2022-09-27 07:59:19 -0500

    Updated juypter on 2022-09-27 07:59:19

---
+++ 05_python_for_devops/calc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+%%bash
+# Processing command line args in a bash script
+#echo "The first arg is $1"
+#echo "input intergers and operator +,-,/, or *"
+#from calc import calc
+import sys
+#if len(sys.argv > 1:
+#    name = sys.argv[1]
+x1 = sys.argv[1]
+x2 = sys.argv[2]
+oper = sys.argv[3]
+#print(sys.argv[0])
+#print(sys.argv[1])
+#print(sys.argv[2])
+print('Program arguments', sys.argv)
+def caculate(x1,x2,oper):
+     
+    try:
+        result = None
+        #oper = input('enter operator: +-/*:')
+        if oper == '+':
+            result = x1 + x2
+        elif oper == '-':
+            result = x1 - x2
+        elif oper == '/':
+            result = x1 / x2
+        elif oper == '*':
+            result = x1 * x2
+    
+    except ZeroDivisionError:
+        print('you cant do this try again')
+        return
+        
+        
+    return result
